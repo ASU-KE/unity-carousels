16dae439764e797e04cef667939751771f0b11c1
John Cox <john.k.cox@asu.edu>
2024-08-16 13:56:35 -0700

    git init

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1b677c52b3dc013b2f02');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'c28d737db0ddcaf6d90e');
