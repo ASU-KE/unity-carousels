91cf3e992daca2cd6c21c7c9bd9234f81a2e108b
John Cox <john.k.cox@asu.edu>
2024-08-16 16:50:48 -0700

    createRoot requires WP 6.2.0

--- unity-carousels.php
+++ unity-carousels.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Unity Carousels
  * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.1
+ * Requires at least: 6.2.0
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            ASU KE Web Services
