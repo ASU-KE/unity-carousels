7f81b25de5b5fe2d274391120f143af24eef0ddc
John Cox <john.k.cox@asu.edu>
2024-08-22 15:16:18 -0700

    build handleAddCard

--- build/card-carousel/index.asset.php
+++ build/card-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '75311b16091584294fec');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b4fa2a5501cb59de1423');
