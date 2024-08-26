7591aa144aa9b535a7b90f2a128afd54302b2db9
John Cox <john.k.cox@asu.edu>
2024-08-26 12:01:02 -0700

    build latest cardHorizontal

--- build/card-carousel/index.asset.php
+++ build/card-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c1a4a084d523e74b512d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '97ce70a65c356e461945');
--- build/card-carousel/view.asset.php
+++ build/card-carousel/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'ae8dd28cc3a1e257082e');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '80100a6955f629e0fcdb');
