63aa36486ae0606eab233cc87b5cc2795af8314f
John Cox <john.k.cox@asu.edu>
2024-08-29 09:22:15 -0700

    build latest removeButton

--- build/card-carousel/index.asset.php
+++ build/card-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6110372e6b2d0a356f84');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7dbf196515416955a22c');
