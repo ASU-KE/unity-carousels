39a2d7ff993e21fe4c254642adda9add4b436fef
John Cox <john.k.cox@asu.edu>
2024-08-30 09:27:03 -0700

    build latest v1.0

--- build/card-carousel/index.asset.php
+++ build/card-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7dbf196515416955a22c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '31d0f4637faeeed92ea1');
