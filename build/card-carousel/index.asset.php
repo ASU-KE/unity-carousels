34c0386a14ed362147488f7f342c3ac4c25c9294
John Cox <john.k.cox@asu.edu>
2024-09-09 16:00:43 -0700

    build latest remove new tab feature

--- build/card-carousel/index.asset.php
+++ build/card-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '31d0f4637faeeed92ea1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7d0deb09d2186ce382ae');
