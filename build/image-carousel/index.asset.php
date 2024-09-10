0d137e2d3cfb85d6caa4be671615ab14d0d96159
John Cox <john.k.cox@asu.edu>
2024-09-10 16:45:34 -0700

    build image-carousel v1.0

---
+++ build/image-carousel/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'add6194b6486ff2bd1a4');
---
+++ build/image-carousel/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '9446dfefc473020fbb00');
