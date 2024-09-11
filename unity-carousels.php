ad7f44be0e2ec21a14184e941642bdfbdc78769a
John Cox <john.k.cox@asu.edu>
2024-09-11 15:25:50 -0700

    register block image-gallery-carousel

--- unity-carousels.php
+++ unity-carousels.php
@@ -28,6 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 function create_block_unity_carousels_block_init() {
 	register_block_type( __DIR__ . '/build/card-carousel' );
 	register_block_type( __DIR__ . '/build/image-carousel' );
+	register_block_type( __DIR__ . '/build/image-gallery-carousel' );
 }
 add_action( 'init', 'create_block_unity_carousels_block_init' );
 /**
