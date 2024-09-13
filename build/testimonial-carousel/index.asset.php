00b7246b70ec296956e4d375bc94f6d3dce98ac7
John Cox <john.k.cox@asu.edu>
2024-09-13 09:13:34 -0700

    build mvp

---
+++ build/testimonial-carousel/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4e6c787a3dcbcd10deaa');
---
+++ build/testimonial-carousel/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '35c0bb62f87e985d4f4b');
