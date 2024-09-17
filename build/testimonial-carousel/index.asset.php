983e42e97efd1ac3abdf977ffd1731c0484be074
John Cox <john.k.cox@asu.edu>
2024-09-16 17:02:22 -0700

    build latest testimonial inspector

--- build/testimonial-carousel/index.asset.php
+++ build/testimonial-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4e6c787a3dcbcd10deaa');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '97ec5dcc18293d7bb439');
