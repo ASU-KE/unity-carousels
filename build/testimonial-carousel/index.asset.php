c0a26e965984fb1b869f58311b62b3d0ffd9fb70
John Cox <john.k.cox@asu.edu>
2024-09-17 14:24:03 -0700

    build latest editor style

--- build/testimonial-carousel/index.asset.php
+++ build/testimonial-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '97ec5dcc18293d7bb439');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5ef42ca68392eab693ab');
