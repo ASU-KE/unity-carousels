5d1a10fa2339615a0d881b421762e6012a011e4d
John Cox <john.k.cox@asu.edu>
2024-09-18 12:25:24 -0700

    card-carousel update v2.1.0

--- build/card-carousel/index.asset.php
+++ build/card-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8b09593f371fbc80d316');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '051741ca073648b8f0e4');
--- build/card-carousel/view.asset.php
+++ build/card-carousel/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '8816aaab909df83821aa');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '8e439b745fe16feb371c');
