b9885623b47cf809f57084efb2e3a468d8514408
John Cox <john.k.cox@asu.edu>
2024-09-24 16:39:22 -0700

    build update v1.1: cite fields, remove img option

--- build/testimonial-carousel/index.asset.php
+++ build/testimonial-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3d9b8ff2d5e60baea81b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'dde6ee8a8b8751ed2067');
